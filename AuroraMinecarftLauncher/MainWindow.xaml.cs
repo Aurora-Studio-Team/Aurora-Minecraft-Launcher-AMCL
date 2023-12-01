4017cfc8fc4f57ca1372e4500338b762b1c7f4e7
FanrklinCN <thz15916108729@163.com>
2023-12-01 21:56:12 +0800

    添加项目文件。

---
+++ AuroraMinecarftLauncher/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using KMCCC.Authentication;
+using KMCCC.Launcher;
+
+
+namespace AuroraMinecarftLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static LauncherCore Core = LauncherCore.Create();
+        public MainWindow()
+        {
+            InitializeComponent();
+            var versions = Core.GetVersions().ToArray();
+            version.ItemsSource = versions;//绑定数据源
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Core.JavaPath = @"C:\Program Files\Java\jdk-18.0.2.1\bin\javaw.exe";
+            var ver = (KMCCC.Launcher.Version)version.SelectedItem;
+            var result = Core.Launch(new LaunchOptions
+            {
+                Version = ver, //Ver为Versions里你要启动的版本名字
+                MaxMemory = 4096, //最大内存，int类型
+                Authenticator = new OfflineAuthenticator("THZ"), //离线启动，ZhaiSoul那儿为你要设置的游戏名
+                                                                      //Authenticator = new YggdrasilLogin("邮箱", "密码", true), // 正版启动，最后一个为是否twitch登录
+                Mode = LaunchMode.MCLauncher, //启动模式，这个我会在后面解释有哪几种
+                // Server = new ServerInfo { Address = "服务器IP地址", Port = "服务器端口" }, //设置启动游戏后，自动加入指定IP的服务器，可以不要
+                // Size = new WindowSize { Height = 768, Width = 1280 } //设置窗口大小，可以不要
+            });
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
+
